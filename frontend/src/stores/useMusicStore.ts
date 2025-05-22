fd13ed6fd3d05275c5cc0295dd557723c5aa4762
TaDat10062002 <talinhdat03@gmail.com>
2025-05-22 18:44:07 +0700

    completed creating_home_page

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -8,8 +8,15 @@ interface MusicStore {
   isLoading: boolean;
   error: string | null;
   currentAlbum: Album | null;
+  madeForYouSongs: Song[];
+  featuredSongs: Song[];
+  trendingSongs: Song[];
   fetchAlbums: () => Promise<void>;
   fetchAlbumById: (id: string) => Promise<void>;
+  fetchTrendingSongs: () => Promise<void>;
+  fetchFeaturedSongs: () => Promise<void>;
+  fetchMadeForYouSongs: () => Promise<void>;
+
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -18,6 +25,9 @@ export const useMusicStore = create<MusicStore>((set) => ({
   isLoading: false,
   error: null,
   currentAlbum: null,
+  featuredSongs: [],
+  trendingSongs: [],
+  madeForYouSongs: [],
 
   fetchAlbums: async () => {
     set({ isLoading: true, error: null });
@@ -42,4 +52,42 @@ export const useMusicStore = create<MusicStore>((set) => ({
       set({ isLoading: false });
     }
   },
+
+  fetchTrendingSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get("/songs/trending");
+      set({ trendingSongs: response.data })
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchFeaturedSongs: async () => {
+    set({ isLoading: true, error: null })
+    try {
+      const response = await axiosInstance.get("/songs/featured");
+      set({ featuredSongs: response.data })
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchMadeForYouSongs: async () => {
+    set({ isLoading: true, error: null })
+    try {
+      const response = await axiosInstance.get("/songs/made-for-you");
+      set({ madeForYouSongs: response.data })
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  }
+
+
 }));
